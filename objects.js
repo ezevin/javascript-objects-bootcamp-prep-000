f6021a5d6ec51f1546a619480fea46de7b9792cb
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-14 21:49:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 const playlist = {'Slowdive': 'Alison',
 'My Bloody Valentine': 'Sometimes'}
-function updatePlaylist(playlist, artistName, songTitle) { Object.assign({'Phil Ochs': "Here's to the State of Mississippi"}
+function updatePlaylist(playlist, artistName, songTitle) { Object.assign({'Phil Ochs': "Here's to the State of Mississippi"})
 }
